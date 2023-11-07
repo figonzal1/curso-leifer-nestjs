c8ae7212bc60a5e7ba56c4e62586f2cab7748b26
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-11-07 17:07:37 -0300

    configuración evennode
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { EjecutivoModule } from './ejecutivo/ejecutivo.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://root:example@localhost:27017/nest?authSource=admin',
+      //'mongodb://root:example@localhost:27017/nest?authSource=admin',
+      'mongodb://58260b04751ebfbd9aaa9cd592d374ac:whj5out7@16a.mongo.evennode.com:27019/58260b04751ebfbd9aaa9cd592d374ac',
     ),
     SucursalModule,
     EjecutivoModule,
